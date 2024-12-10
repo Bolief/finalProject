012d6647b6045517e1c5b045429025b295a58aee
francis.bolie <francis.bolie@5CD2452PWK>
2024-12-09 20:36:08 -0600

    make chages

---
+++ finalProject/Controllers/BattleController.cs
@@ -0,0 +1,53 @@
+﻿using finalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace finalProject.Controllers
+{
+    public class BattleController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BattleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var teams = _context.Teams.Include(t => t.Characters).ToList();
+            return View(teams);
+        }
+
+        public IActionResult Battle(int team1Id, int team2Id)
+        {
+            var team1 = _context.Teams.Include(t => t.Characters).FirstOrDefault(t => t.Id == team1Id);
+            var team2 = _context.Teams.Include(t => t.Characters).FirstOrDefault(t => t.Id == team2Id);
+
+            if (team1 == null || team2 == null)
+            {
+                TempData["ErrorMessage"] = "One or both teams could not be found.";
+                return RedirectToAction("Index");
+            }
+
+            // Simple battle logic (you can enhance this)
+            var winner = (team1.TotalWins > team2.TotalWins) ? team1 : team2;
+
+            // Save battle to database
+            var battle = new Battle
+            {
+                Team1Id = team1.Id,
+                Team2Id = team2.Id,
+                WinnerTeamId = winner.Id,
+                BattleDate = DateTime.Now
+            };
+            _context.Battles.Add(battle);
+            _context.SaveChanges();
+
+            ViewBag.Winner = winner;
+            return View(battle);
+        }
+
+    }
+
+}
---
+++ finalProject/Models/Battle.cs
@@ -0,0 +1,15 @@
+﻿namespace finalProject.Models
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public int Team1Id { get; set; }
+        public Team Team1 { get; set; }
+        public int Team2Id { get; set; }
+        public Team Team2 { get; set; }
+        public int WinnerTeamId { get; set; }
+        public Team Winner { get; set; }
+        public DateTime BattleDate { get; set; }
+    }
+
+}
