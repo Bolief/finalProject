e38a904af74c8428554f84a03f953763a0a8e870
Kex6awn <fferdinard1@gmail.com>
2024-12-12 02:44:26 -0600

    Login and Registration Pages

--- finalProject/Controllers/UsersController.cs
+++ finalProject/Controllers/UsersController.cs
@@ -16,5 +16,52 @@ public class UsersController : Controller
         var users = _context.Users.Include(u => u.Teams).ToList();
         return View(users);
     }
+
+    //Login and Registration Methods
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Implement authentication logic
+            return RedirectToAction("Index");
+        }
+        return View(model);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var newUser = new User
+            {
+                Username = model.Username,
+                TotalWins = 0, // New users start with 0 wins
+                Teams = new List<Team>(), // Empty team list
+                Badges = new List<Badge>() // Empty badge list
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
+        }
+        return View(model);
+    }
+
 }
+
+
 
