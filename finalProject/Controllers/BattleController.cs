86b527fb9d1c56d061a2cad6a28c83ceaea01497
Kex6awn <fferdinard1@gmail.com>
2024-12-12 14:10:17 -0600

    Battle Logic Test

--- finalProject/Controllers/BattleController.cs
+++ finalProject/Controllers/BattleController.cs
@@ -30,23 +30,48 @@ namespace finalProject.Controllers
                 return RedirectToAction("Index");
             }
 
-            // battle logic 
-            var winner = (team1.TotalWins > team2.TotalWins) ? team1 : team2;
+            // Calculate team combat scores
+            double CalculateTeamScore(Team team)
+            {
+                return team.Characters.Sum(c =>
+                    (c.Strength * 0.4) +  // Weight strength
+                    (c.Defense * 0.3) +  // Weight defense
+                    (c.Speed * 0.2) +    // Weight speed
+                    (c.Health * 0.1)     // Weight health
+                );
+            }
 
-            // Save battle to database
+            var team1Score = CalculateTeamScore(team1);
+            var team2Score = CalculateTeamScore(team2);
+
+            Team? winner = null;
+            if (team1Score > team2Score)
+            {
+                winner = team1;
+            }
+            else if (team2Score > team1Score)
+            {
+                winner = team2;
+            }
+            // winner remains null if it's a tie
+
+            // Save the battle result to the database
             var battle = new Battle
             {
                 Team1Id = team1.Id,
                 Team2Id = team2.Id,
-                WinnerTeamId = winner.Id,
+                WinnerTeamId = winner?.Id, // Allow null if tied
                 BattleDate = DateTime.Now
             };
             _context.Battles.Add(battle);
             _context.SaveChanges();
 
             ViewBag.Winner = winner;
+            ViewBag.Team1Score = team1Score;
+            ViewBag.Team2Score = team2Score;
             return View(battle);
         }
+
 
     }
 
